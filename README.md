8e680dfa9d786623a5aef7909f8f17ef080eca59
aleks4ay <aleks4ay@gmail.com>
2025-07-26 13:04:56 +0300

    Added testContainer

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ Part of Hotel microservices
 ## Liquibase migrations
 - Переливка осуществляется при запуске приложения
 - Перелитые данные повторно не заливаются
+
+## Testes
+
+- Для тестирования был добавлен профиль `@ActiveProfiles("test")` и добавлен файл с настройками `application-test.yml`.
+  Настройки будут браться из основного файла `application.yml`, потом перезатираться взятыми из `application-test.yml`.
+- В конце еще настройки подключения к БД будут перезатерты данными, полученными от контейнера
+    - "spring.datasource.url", sqlServerContainer::getJdbcUrl
+    - "spring.datasource.username", sqlServerContainer::getUsername
+    - "spring.datasource.password", sqlServerContainer::getPassword
+- Для конкретного примера можно было профиль и `application-test.yml` не добавлять, так как ни одна из переменных не отличается от тех, что в `application.yml`
---
+++ src/test/java/com/aleks4ay/DatabaseMigrationTest.java
@@ -0,0 +1,46 @@
+package com.aleks4ay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+public class DatabaseMigrationTest {
+    @Container
+    static MSSQLServerContainer<?> sqlServerContainer = new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:2019-latest")
+            .acceptLicense();
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", sqlServerContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", sqlServerContainer::getUsername);
+        registry.add("spring.datasource.password", sqlServerContainer::getPassword);
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testMigrationsApplied() throws Exception {
+        String query = "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'test_table'";
+        try (Connection conn = dataSource.getConnection(); ResultSet rs = conn.createStatement().executeQuery(query)) {
+            rs.next();
+            int count = rs.getInt(1);
+            assertThat(count).isEqualTo(1);
+        }
+    }
+}
